358834e12a2278daa98ca3870bf1ddbc9388a41c
eriknathan <eriknathan.contato@gmail.com>
2025-07-20 11:56:44 -0300

    feat: all crud - brand

--- brands/urls.py
+++ brands/urls.py
@@ -3,5 +3,13 @@ from . import views
 
 
 urlpatterns = [
-    path('brands/lists/', views.BrandListView.as_view(), name='brand_list')
+    path('brands/lists/', views.BrandListView.as_view(), name='brand_list'),
+    path('brands/create/', views.BrandCreateView.as_view(),
+         name='brand_create'),
+    path('brands/<int:pk>/detail/', views.BrandDetailView.as_view(),
+         name='brand_detail'),
+    path('brands/<int:pk>/update/', views.BrandUpdateView.as_view(),
+         name='brand_update'),
+    path('brands/<int:pk>/delete/', views.BrandDeleteView.as_view(),
+         name='brand_delete'),
 ]
